d74683d46a78e19d59a5648a47873a518fd03a5b
Iuliia Tsaplina <zhydanovajulia@gmail.com>
2019-10-03 19:21:14 +0300

    Improve devise UI forms.

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,12 @@ module DocRegister
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    config.to_prepare do
+      Devise::SessionsController.layout "devise"
+      Devise::RegistrationsController.layout "devise"
+      Devise::ConfirmationsController.layout "devise"
+      Devise::UnlocksController.layout "devise"
+      Devise::PasswordsController.layout "devise"
+    end
   end
 end
