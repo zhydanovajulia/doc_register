2c11f430ebb8941f1513fa8d3d94887576788237
Iuliia Tsaplina <zhydanovajulia@gmail.com>
2019-10-03 18:51:34 +0300

    add user document creation test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,7 +5,30 @@ RSpec.describe User, type: :model do
   it { should have_db_column(:email) }
   it { should have_db_column(:encrypted_password) }
 
-  describe "Validations" do
-    it { should validate_presence_of(:email) }
+  it { should validate_presence_of(:email) }
+
+
+  context "creates document" do
+    let(:user) { create :user }
+
+    it "without attachments" do
+      doc = user.documents.create name: "New doc", title: "Important!", description: "Description"
+
+      expect(user.reload.documents).to eq([doc])
+      expect(Document.all.count).to eq(1)
+      expect(doc.user).to eq(user)
+    end
+
+    it "with attachments" do
+      file_pdf = Rails.root.join("spec/support/story.pdf").to_s
+      file = File.open(file_pdf)
+
+      doc = create :document
+      doc.attachments = file
+
+      expect(doc.reload.attachments.count).to eq(1)
+      expect(doc.attachments.last.file_name).to eq("story.pdf")
+    end
+
   end
 end
